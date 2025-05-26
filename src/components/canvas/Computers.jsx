4a054afe8de9ce69db82a9a41938ebbfcf9e9f12
Haad majeed <haadmajeedch27@gmail.com>
2025-05-26 19:10:32 +0500

    computer and hero part done ALLHAMDUILLAH

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,24 +1,55 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../Loader'
+import React, { useState, useEffect, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-// this component loads the 3D model of the computer
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+// Component to load the 3D model of the computer
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      {/* this point light is the moving light as we move the model */}
+      <hemisphereLight intensity={0.35} groundColor="gray" />
       <pointLight intensity={1} />
-      {/* load the glTF model as a primitive object */}
-      <primitive object={computer.scene} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1.5}
+        castShadow
+        shadow-mapSize={2048}
+      />
+      <directionalLight position={[5, 10, 7]} intensity={0.8} castShadow shadow-mapSize={2048} />
+      <directionalLight position={[7, 30, 7]} intensity={0.8} castShadow shadow-mapSize={2048} />
+      <directionalLight position={[15, 15, 7]} intensity={0.8} castShadow shadow-mapSize={2048} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.45 : 0.75}
+        position={isMobile ? [-2.5, -2.5, -1.0] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+const mediaQuery = window.matchMedia(`(1500 px)`);
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -26,19 +57,14 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      {/* canvas loader brings the loader when the model is loading */}
       <Suspense fallback={<CanvasLoader />}>
-        {/* restrict the rotation only to the horizontal axis (left-right) using min and max polar angle */}
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers />
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        <Computers isMobile={isMobile} />
         <Preload all />
       </Suspense>
     </Canvas>
-  )
-}
+  );
+};
 
-export default ComputersCanvas
+
+export default ComputersCanvas;
