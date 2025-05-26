10d3c94c3b6c276ed0f61b814ffa84aa6d952516
Haad majeed <haadmajeedch27@gmail.com>
2025-05-26 11:48:15 +0500

    the first 3d model in action

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    // this section is the one that makes the background full over the screen
+    // the div bellow this is use for making the purple dot and the gradient line fading out
+    <section className="realtive w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+           <div className = "w-5 h-5 rounded-full bg-[#915eff]"></div>
+           <div className='w-1 sm:h-80 h-40 violet-gradient'></div>
+        </div>
+        {/* this is reponsible for making the text all fancy and good */}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Haad</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I am learning software development <br className='sm:block hidden'/>and web applications</p>
+        </div>
+      </div>
+
+
+        <ComputersCanvas>
+
+        </ComputersCanvas>
+    </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
+// this component loads the 3D model of the computer
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      {/* this point light is the moving light as we move the model */}
+      <pointLight intensity={1} />
+      {/* load the glTF model as a primitive object */}
+      <primitive object={computer.scene} />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* canvas loader brings the loader when the model is loading */}
+      <Suspense fallback={<CanvasLoader />}>
+        {/* restrict the rotation only to the horizontal axis (left-right) using min and max polar angle */}
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
