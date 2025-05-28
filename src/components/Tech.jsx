68fe99603d9674dfb03bf2399f43acc70f62c4ac
Haad majeed <haadmajeedch27@gmail.com>
2025-05-28 15:49:34 +0500

    start of tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,27 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <div className="flex flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-
-    </div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Technologies I Know</p>
+        <h2 className={styles.sectionHeadText}>Skills.</h2>
+      </motion.div>
+      
+      <div className="flex flex-wrap justify-center gap-10 mt-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default SectionWrapper(Tech, "");
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+import {github} from "../assets";
+import {projects} from "../constants"
+import { styles } from "../styles";
 
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 const Works = () => {
   return (
-    <div>Works</div>
+    // as we are wrapping this in sectionwrapper so w ehave to use a empty <> </> a empty fragment
+   <>
+       <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+   
+   </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
